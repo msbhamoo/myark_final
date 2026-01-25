fa56e601b5ae05d4001b306a3c5359ed787c5d27
msbhamoo <mahendrabhamoo@gmail.com>
2026-01-25 16:29:31 +0530

    fix: remove conflicting next.js config files

--- .next/dev/static/chunks/src_app_page_tsx_444e0937._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_d8b24c49._.js",
-  "static/chunks/node_modules_b07e1249._.js",
-  "static/chunks/src_30b28a1e._.css"
-],
-    source: "dynamic"
-});
